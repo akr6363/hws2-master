47ebbfe25144ef118ce19f8dca2edfbab0c0e6cd
Anna Krasilnikova <akr6363@mail.ru>
2023-04-17 22:37:35 +0300

    hw8

--- src/s2-homeworks/hw08/bll/homeWorkReducer.ts
+++ src/s2-homeworks/hw08/bll/homeWorkReducer.ts
@@ -4,15 +4,16 @@ type ActionType =
     | { type: 'sort'; payload: 'up' | 'down' }
     | { type: 'check'; payload: number }
 
-export const homeWorkReducer = (state: any, action: any): any => { // need to fix any
+export const homeWorkReducer = (state: UserType[], action: ActionType): UserType[] => { // need to fix any
     switch (action.type) {
         case 'sort': { // by name
-
-            return state // need to fix
+            const sortCopy = [...state].sort((a, b) => a.name.localeCompare(b.name))
+            return  action.payload === 'up'
+            ? sortCopy
+            : sortCopy.reverse()// need to fix
         }
         case 'check': {
-
-            return state // need to fix
+            return state.filter((u) => u.age >= 18)
         }
         default:
             return state
