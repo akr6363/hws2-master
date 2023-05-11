675752b5a9b97ccae51cf49907a24e4d4598c2bb
Anna Krasilnikova <akr6363@mail.ru>
2023-05-11 23:30:51 +0300

    hw11

--- src/s2-homeworks/hw11/HW11.tsx
+++ src/s2-homeworks/hw11/HW11.tsx
@@ -15,7 +15,15 @@ function HW11() {
     const [value1, setValue1] = useState(restoreState<number>('hw11-value1', 0))
     const [value2, setValue2] = useState(restoreState<number>('hw11-value2', 100))
 
-    const change = (event: any, value: any) => {
+    const change = (event: Event, value: number | number[]) => {
+        if (Array.isArray(value)) {
+            // Если значение - массив, то сохраняем в оба состояния
+            setValue1(value[0])
+            setValue2(value[1])
+        } else {
+            // Иначе сохраняем только в первое состояние
+            setValue1(value)
+        }
         // пишет студент // если пришёл массив - сохранить значения в оба useState, иначе в первый
     }
 
@@ -30,15 +38,17 @@ function HW11() {
                         <SuperRange
                             id={'hw11-single-slider'}
                             // сделать так чтоб value1 изменялось // пишет студент
-
+                            value={value1}
+                            onChange={change}
                         />
                     </div>
                     <div className={s.wrapper}>
                         <span id={'hw11-value-1'} className={s.number}>{value1}</span>
                         <SuperRange
                             id={'hw11-double-slider'}
+                            value={[value1, value2]}
+                            onChange={change}
                             // сделать так чтоб value1/2 изменялось // пишет студент
-
                         />
                         <span id={'hw11-value-2'} className={s.number}>{value2}</span>
                     </div>
--- src/s2-homeworks/hw11/common/c7-SuperRange/SuperRange.tsx
+++ src/s2-homeworks/hw11/common/c7-SuperRange/SuperRange.tsx
@@ -1,11 +1,33 @@
 import React from 'react'
 import {Slider, SliderProps} from '@mui/material'
+import shadows from "@mui/material/styles/shadows";
 
 const SuperRange: React.FC<SliderProps> = (props) => {
     return (
         <Slider
             sx={{ // стили для слайдера // пишет студент
-                
+                width: '150px',
+                color: '#00CC22',
+                margin: '0 12px',
+                '& .MuiSlider-thumb': {
+                    border: '2px solid #00CC22',
+                    width: '18px',
+                    height: '18px',
+                    background: '#fff',
+                    '&:before': {
+                        display: 'block',
+                        width: '6px',
+                        height: '6px',
+                        border: 'none',
+                        boxShadow: 'none',
+                        backgroundColor: '#00CC22',
+
+                    },
+                },
+                '& .MuiSlider-rail': {
+                    backgroundColor: '#8B8B8B',
+                    opacity: 1
+                },
             }}
             {...props} // отдаём слайдеру пропсы если они есть (value например там внутри)
         />
